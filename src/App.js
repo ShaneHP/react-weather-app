d62c98786287d22b6ea4304720745e4272bd6729
Shane Hyland Pierce <shanehp98@gmail.com>
2021-09-23 12:38:03 +0100

    added API call and stored result in state variable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [lat, setLat] = useState([]);
+    const [long, setLong] = useState([]);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            navigator.geolocation.getCurrentPosition((position) => {
+                setLat(position.coords.latitude);
+                setLong(position.coords.longitude);
+            });
+
+            console.log('Latitude is:', lat);
+            console.log('Longitude is:', long);
+
+            /* async/await method */
+            const response = await fetch(
+                `${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+            );
+            const responseJSON = await response.json();
+            setData(responseJSON);
+            console.log(responseJSON);
+            /* .then() method */
+
+            // fetch(
+            //     `${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+            // )
+            //     .then((res) => res.json())
+            //     .then((result) => {
+            //         setData(result);
+            //         console.log(result);
+            //     });
+        };
+        fetchData();
+    }, [lat, long]);
+
+    return <div className="App"></div>;
 }
 
 export default App;
